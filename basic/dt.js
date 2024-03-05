a9824f8dcaca8dc80f972ca2dabcdbf4446f1305
Manodra Bhumin <146631108+Bhumin31@users.noreply.github.com>
2024-03-05 06:56:18 +0000

    datatype conversion

---
+++ basic/dt.js
@@ -0,0 +1,16 @@
+let num = "33abc"
+console.log(typeof num);
+let val = Number(num);
+console.log(typeof val);
+console.log(val);
+
+// NaN = Not a number
+
+let isLog = "yo"
+let boolIsLog = Boolean(isLog);
+console.log(boolIsLog);
+
+// "" = false
+// "anything" = true
+// 1 = true
+// 0 = false;
